9d81b1eaa2c13a1d676b122fcf691ba79129d1ff
luiz.camargo <luiz.camargo@4RSISTEMAS.BR>
2021-09-23 16:59:17 -0300

    Gerador de Senhas

---
+++ teste2.py
@@ -0,0 +1,17 @@
+import random
+
+#GERADOR DE SENHAS
+
+minusculo = ("abcdefghjklmnopqrstuvwxyz")
+maiusculo = ("ABCDEFGHJKLMNOPQRSTUVWXYZ")
+numeros = ("0123456789")
+simbolos = ("@#$%&*")
+
+
+soma = minusculo + maiusculo + numeros + simbolos
+
+tamanho = 5
+senha = random.sample(soma,tamanho)
+print("".join(senha))
+
+
