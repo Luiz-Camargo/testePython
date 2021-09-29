4b3bba3d1ce748e8082479c29d8b8f9f65628618
Luiz-Camargo <luiz.henriquecamargo@hotmail.com>
2021-09-29 17:59:27 -0300

    Continuação teste 4 | Teste de tela

--- teste4.py
+++ teste4.py
@@ -10,9 +10,9 @@ driver = webdriver.Chrome()
 driver.get('https://selenium.dunossauro.live/todo_list.html')
 
 tarefas = ["Testar OS 100", "Testar OS 101",
-           "Testar OS 102", "Testar OS 103", "Testar OS 104"]
+           "Testar OS 102", "Testar OS 103", "Testar OS 104", "Testar OS 105"]
 
-descricoes = ["Testar a OS de acordo com especificações.", "Testar a OS printando todas as evidências.", "Testar a OS de acordo com esperado.", "Testar a OS de acordo.", "Testar a OS de acordo com as especificações esperadas."]
+descricoes = ["Testar a OS de acordo com especificações.", "Testar a OS printando todas as evidências.", "Testar a OS de acordo com esperado.", "Testar a OS de acordo.", "Testar a OS de acordo com as especificações esperadas.", "Realizar os testes de acordo com o especificado."]
 
 tarefa = driver.find_element_by_xpath("//input[@id='todo-name']")
 tarefa.send_keys(tarefas[0])
@@ -67,4 +67,34 @@ descricao.send_keys(descricoes[4])
 fila = driver.find_element_by_xpath("//button[@id='todo-submit']")
 fila.click()
 
-time.sleep(10)
+time.sleep(3)
+
+tarefa = driver.find_element_by_xpath("//input[@id='todo-name']")
+tarefa.send_keys(tarefas[5])
+
+descricao = driver.find_element_by_id("todo-desc")
+descricao.send_keys(descricoes[5])
+
+urgente = driver.find_element_by_xpath("//input[@id='todo-next']")
+urgente.click()
+
+fila = driver.find_element_by_xpath("//button[@id='todo-submit']")
+fila.click()
+
+time.sleep(5)
+
+fazer = driver.find_element_by_xpath("//button[contains(text(),'Fazer')]")
+fazer.click()
+
+cancelar = driver.find_element_by_xpath("//button[contains(text(),'Cancelar')]")
+cancelar.click()
+
+time.sleep(3)
+
+pronto = driver.find_element_by_xpath("//button[contains(text(),'Pronto')]")
+pronto.click()
+
+time.sleep(3)
+
+refazer = driver.find_element_by_xpath("//button[contains(text(),'Refazer')]")
+refazer.click()
--- testetela.py
+++ testetela.py
@@ -1,15 +1,59 @@
-import PySimpleGUI as sg
-from PySimpleGUI.PySimpleGUI import BLUES, theme_text_color
+# GERADOR DE SENHAS
+import random
+from PySimpleGUI import PySimpleGUI as sg
 
+# Declarando as váriaveis
+minusculo = ("abcdefghjklmnopqrstuvwxyz")
+maiusculo = ("ABCDEFGHJKLMNOPQRSTUVWXYZ")
+numeros = ("0123456789")
+simbolos = ("@#$%&*")
+
+# Juntando as variáveis e pegando o tamanho e número de senhas que devem ser geradas
+varjuntas = minusculo + maiusculo + numeros + simbolos
+tamanho = ("")
+nmrsenhas = ("")
+
+# Layout da tela
 sg.theme('DarkBlue')
-
 layout = [
-    [sg.Text('GERADOR DE SENHAS')],
-    [sg.Text('Insira o tamanho da senha:'), sg.InputText()],
-    [sg.Text('Insira o número de senhas que deve ser gerada:'), sg.InputText()],    
-    [sg.Button('Gerar Senha'), sg.Button('Cancelar')]]
+    [sg.Text('Tamanho da senha:')],
+    [sg.Input(tamanho)],
+    [sg.Text('Número de senhas que serão geradas:')],
+    [sg.Input(nmrsenhas)],
+    [sg.Text('')],
+    [sg.Button('Gerar Senha'), sg.Button('Cancelar')]
+]
 
-window = sg.Window('Gerador de Senhas', layout)
-event, values = window.read()
-window.close()
-print(event, values[0], values[1])
+# Janela que abrirá
+janela = sg.Window('GERADOR DE SENHAS', layout)
+
+# Ler os eventos que devem ocorrer
+while True:
+    evento, valores = janela.read()
+    if evento == sg.WINDOW_CLOSED or 'Cancelar':
+        break
+    if evento == 'Gerar Senha':
+        for senha in range(int(valores = nmrsenhas)):
+            senha = random.sample(varjuntas, int(valores = tamanho))
+            print("".join(senha))
+
+#import PySimpleGUI as sg
+#
+#def ChatBot():
+#    layout = [[(sg.Text('This is where standard out is being routed', size=[40, 1]))],
+#              [sg.Output(size=(80, 20))],
+#              [sg.Multiline(size=(70, 5), enter_submits=True),
+#               sg.Button('SEND', button_color=(sg.YELLOWS[0], sg.BLUES[0])),
+#               sg.Button('EXIT', button_color=(sg.YELLOWS[0], sg.GREENS[0]))]]
+#
+#    window = sg.Window('Chat Window', layout, default_element_size=(30, 2))
+#
+#    # ---===--- Loop taking in user input and using it to query HowDoI web oracle --- #
+#    while True:
+#        event, value = window.read()
+#        if event == 'SEND':
+#            print(value)
+#        else:
+#            break
+#    window.close()
+#ChatBot()
